64d8152e76d9b3a4228fd2f55c96ec435f3245aa
Samaun <samaunkhan77@gmail.com>
2025-01-01 23:15:44 +0600

    Initial commit

---
+++ bin/test1.dart
@@ -0,0 +1,15 @@
+
+void triangle() {
+  double base = 5000;
+  double height = 500;
+
+  double area = 0.5 * base * height;
+
+  print('The base of the triangle is: $base');
+  print('The height of the triangle is: $height');
+  print('The area of the triangle is: $area');
+}
+
+void main() {
+  triangle();
+}
